65113ba6d5e2bba74d2392b36e9a886c501b67b3
prdcut <danielfma10@gmail.com>
2022-11-17 19:35:04 +0100

    Simplify endpoints for user based model

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose');
 
-const { check, validationResult } = require('express-validator');
+const { check, validationResult, body } = require('express-validator');
 const app = express();
 const Models = require('./models.js');
-const Food = Models.Food;
 const User = Models.User;
+// const FoodDiary = Models.FoodDiary;
+// const NutirionalCont = Models.NutirionalCont;
+const Food = Models.Food;
 
 // Middlewear
 
@@ -15,26 +17,26 @@ const User = Models.User;
   Uncomment here to run project local ⬇️
 */
 
-// mongoose.connect('mongodb://localhost:27017/foodtrack', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect('mongodb://127.0.0.1/foodtrack', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 /* 
   Comment out here if running project local ⬇️
 */
 
 // Make sure to set before any app.use
-mongoose.connect(process.env.CONNECTION_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.CONNECTION_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 // let allowedOrigins = [
 //   'http://localhost:8080',
 //   'https://foodtrackbackend.herokuapp.com/',
@@ -74,10 +76,10 @@ app.get(
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Food.find()
-      .then(food => {
+      .then((food) => {
         res.status(201).json(food);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
         res.status(500).send(`Error ${error}`);
       });
@@ -89,11 +91,11 @@ app.get(
   '/food-list/:name',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    Food.findOne({ name: req.body.name })
-      .then(food => {
+    Food.findOne({ name: req.params.name })
+      .then((food) => {
         res.json(food);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
         res.status(500).send(`Error ${error}`);
       });
@@ -106,7 +108,7 @@ app.post(
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Food.findOne({ name: req.body.name })
-      .then(food => {
+      .then((food) => {
         if (food) {
           return res.status(400).send(`${req.body.name} already exists`);
         } else {
@@ -114,23 +116,21 @@ app.post(
             name: req.body.name,
             weight: req.body.weight,
             quantity: req.body.quantity,
-            macros: {
-              protein: req.body.macros.protein,
-              carbs: req.body.macros.carbs,
-              fat: req.body.macros.fat,
-              calories: req.body.macros.calories,
-            },
+            protein: req.body.protein,
+            carbs: req.body.carbs,
+            fat: req.body.fat,
+            calories: req.body.calories,
           })
-            .then(food => {
+            .then((food) => {
               res.status(201).json(food);
             })
-            .catch(error => {
+            .catch((error) => {
               console.error(error);
               res.status(500).send(`Error ${error}`);
             });
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
         res.status(500).send(`Error ${error}`);
       });
@@ -143,14 +143,14 @@ app.delete(
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Food.findOneAndRemove({ name: req.params.name })
-      .then(food => {
+      .then((food) => {
         if (!food) {
           res.status(400).send(`${req.params.name} was not found.`);
         } else {
           res.status(200).send(`${req.params.name} was deleted.`);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
         res.status(500).send(`Error ${error}`);
       });
@@ -159,7 +159,7 @@ app.delete(
 
 // Updates food data of a food by name
 app.put(
-  '/food-list/:name/:macro/:value',
+  '/food-list/:name',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Food.findOneAndUpdate(
@@ -169,12 +169,10 @@ app.put(
           name: req.body.name,
           weight: req.body.weight,
           quantity: req.body.quantity,
-          macros: {
-            protein: req.body.protein,
-            carbs: req.body.carbs,
-            fat: req.body.fat,
-            calories: req.body.calories,
-          },
+          protein: req.body.protein,
+          carbs: req.body.carbs,
+          fat: req.body.fat,
+          calories: req.body.calories,
         },
       },
       { new: true },
@@ -187,22 +185,6 @@ app.put(
         }
       }
     );
-  }
-);
-
-// Returns user profile
-app.get(
-  '/users/:username',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    User.findOne({ username: req.params.username })
-      .then(user => {
-        res.status(201).json(user);
-      })
-      .catch(error => {
-        console.log(error);
-        res.status(500).send(`Error ${error}`);
-      });
   }
 );
 
@@ -225,38 +207,52 @@ app.post(
     }
 
     let hashedPassword = User.hashPassword(req.body.password);
-    User.findOne({ email: req.body.username })
-      .then(user => {
+    User.findOne({ username: req.body.username })
+      .then((user) => {
         if (user) {
           return res.status(400).send(`${req.body.email} already exists`);
         } else {
           User.create({
-            email: req.body.email,
-            password: hashedPassword,
-            gender: req.body.gender,
             username: req.body.username,
-            age: req.body.age,
+            password: hashedPassword,
+            email: req.body.email,
+            birthdate: req.body.birthdate,
+            sex: req.body.sex,
             height: req.body.height,
-            currentWeight: req.body.currentWeight,
-            goalWeight: req.body.goalWeight,
           })
-            .then(user => {
+            .then((user) => {
               res.status(201).json(user);
             })
-            .catch(error => {
+            .catch((error) => {
               console.log(error);
               res.status(500).send(`Error ${error}`);
             });
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
         res.status(500).send(`Error ${error}`);
       });
   }
 );
 
-// Allows existing user to update their info
+// Returns user profile
+app.get(
+  '/users/:username',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    User.findOne({ username: req.params.username })
+      .then((user) => {
+        res.status(201).json(user);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send(`Error ${error}`);
+      });
+  }
+);
+
+// Allows existing user to update basic info
 app.put(
   '/users/:username',
   passport.authenticate('jwt', { session: false }),
@@ -266,8 +262,16 @@ app.put(
       {
         $set: {
           username: req.body.username,
-          password: req.body.password,
-          email: req.params.email,
+          email: req.body.email,
+          birthdate: req.body.birthdate,
+          sex: req.body.sex,
+          height: req.body.height,
+          macros: {
+            protein: req.body.protein,
+            carbs: req.body.carbs,
+            fat: req.body.fat,
+            calories: req.body.calories,
+          },
         },
       },
       { new: true },
@@ -283,18 +287,31 @@ app.put(
   }
 );
 
-// Allows user to add food to their list of meals
+// Allows user to add a food diary entry to profile
 app.post(
-  '/users/:username/meals/:foodId',
+  '/users/:username/foodDiary',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     User.findOneAndUpdate(
       { username: req.params.username },
-      { $addToSet: { meals: req.params.foodId } },
+      {
+        $push: {
+          foodDiary: {
+            date: req.body.date,
+            diaryMeal: req.body.diaryMeal,
+            nutritionalCont: {
+              protein: req.body.diaryProtein,
+              carbs: req.body.diaryCarbs,
+              fat: req.body.diaryFat,
+              calories: req.body.diaryCalories,
+            },
+          },
+        },
+      },
       { new: true },
       (error, updatedUser) => {
         if (error) {
-          console.log(error);
+          console.error(error);
           res.status(500).send(`Error ${error}`);
         } else {
           res.json(updatedUser);
@@ -304,18 +321,142 @@ app.post(
   }
 );
 
-// Allows user to remove food from their list of meals
+// Allows user to update a food entry from diary
+app.put(
+  '/users/:username/:foodDiaryId',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    User.findOneAndUpdate(
+      {
+        username: req.params.username,
+        'foodDiary._id': req.params.foodDiaryId,
+      },
+      {
+        $set: {
+          'foodDiary.$.date': req.body.date,
+          'foodDiary.$.diaryMeal': req.body.diaryMeal,
+          'foodDiary.$.nutritionalCont.protein': req.body.diaryProtein,
+          'foodDiary.$.nutritionalCont.carbs': req.body.diaryCarbs,
+          'foodDiary.$.nutritionalCont.fat': req.body.diaryFat,
+          'foodDiary.$.nutritionalCont.calories': req.body.diaryCalories,
+        },
+      },
+      { new: true },
+      (error, updatedUser) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send(`Error ${error}`);
+        } else {
+          res.json(updatedUser);
+        }
+      }
+    );
+  }
+);
+
+// Allows user to delete a food diary entry from profile
 app.delete(
-  '/users/:username/meals/:foodId',
+  '/users/:foodDiaryId',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    User.findOneAndUpdate(
+      {
+        'foodDiary._id': req.params.foodDiaryId,
+      },
+      {
+        $pull: {
+          foodDiary: { _id: req.params.foodDiaryId },
+        },
+      },
+      { new: true },
+      (error, updatedUser) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send(`Error ${error}`);
+        } else {
+          res.json(updatedUser);
+        }
+      }
+    );
+  }
+);
+
+// Allows user to add a weight diary entry to profile
+app.post(
+  '/users/:username/weightDiary',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     User.findOneAndUpdate(
       { username: req.params.username },
-      { $pull: { meals: req.params.foodId } },
+      {
+        $push: {
+          weightDiary: {
+            date: req.body.weightDate,
+            value: req.body.weightValue,
+          },
+        },
+      },
       { new: true },
       (error, updatedUser) => {
         if (error) {
-          console.log(error);
+          console.error(error);
+          res.status(500).send(`Error ${error}`);
+        } else {
+          res.json(updatedUser);
+        }
+      }
+    );
+  }
+);
+
+// Allows user to update a weight entry from diary
+app.put(
+  '/users/:username/:weightDiaryId',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    User.findOneAndUpdate(
+      {
+        username: req.params.username,
+        'weightDiary._id': req.params.weightDiaryId,
+      },
+      {
+        $set: {
+          'weightDiary.$.date': req.body.date,
+          'weightDiary.$.value': req.body.value,
+        },
+      },
+      { new: true },
+      (error, updatedUser) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send(`Error ${error}`);
+        } else {
+          res.json(updatedUser);
+        }
+      }
+    );
+  }
+);
+
+// Allows user to delete a weight diary entry from profile
+app.delete(
+  '/users/:weightDiaryId',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    User.findOneAndUpdate(
+      {
+        username: req.params.username,
+        'weightDiary._id': req.params.weightDiaryId,
+      },
+      {
+        $pull: {
+          weightDiary: { _id: req.params.weightDiaryId },
+        },
+      },
+      { new: true },
+      (error, updatedUser) => {
+        if (error) {
+          console.error(error);
           res.status(500).send(`Error ${error}`);
         } else {
           res.json(updatedUser);
@@ -327,19 +468,24 @@ app.delete(
 
 // Allows user to deregister
 app.delete(
-  '/users/:username/',
+  '/users/:username/:email',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    User.findOneAndRemove({ username: req.params.username })
-      .then(user => {
+    User.findOneAndRemove({
+      username: req.params.username,
+      email: req.params.email,
+    })
+      .then((user) => {
         if (!user) {
           res.status(400).send(`${req.params.username} was not found`);
         } else {
-          res.status(200).send(`${req.params.username} was deleted`);
+          res
+            .status(200)
+            .send(`${req.params.username} was successfully deleted`);
         }
       })
-      .catch(err => {
-        console.error(err);
+      .catch((error) => {
+        console.error(error);
         res.status(500).send(`Error ${error}`);
       });
   }
@@ -351,16 +497,16 @@ app.delete(
   Comment out here if running project local ⬇️
 */
 
-const port = process.env.PORT || 8080;
+// const port = process.env.PORT || 8080;
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Listening on Port ${port}`);
-});
+// app.listen(port, '0.0.0.0', () => {
+//   console.log(`Listening on Port ${port}`);
+// });
 
 /* 
   Uncomment here to run project local
 */
 
-// app.listen(8080, () => {
-//   console.log('Your app is listening on port 8080');
-// });
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080');
+});
